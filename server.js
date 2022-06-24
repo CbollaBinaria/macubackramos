0d89d91e50b3a46cd0cd086b70d462819d731d74
Sebastian Ramos <sebastianalejandroramos96@gmail.com>
2022-06-24 15:43:38 -0300

    Desafio express

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+
+class Contenedor {
+    constructor(file){
+        this.file = file
+    }
+
+    getAll(){
+        const data = JSON.parse(fs.readFileSync(this.file, 'utf-8'))
+        console.log(data)
+        return data
+    }
+    saveProduct(product){
+        let listado = this.getAll();
+        product.id = listado.length+1        
+        listado.push(product)
+        fs.writeFile(this.file, `${JSON.stringify(listado)}`, error => {
+            if (error){
+                console.log('Hubo un problema al guardar el archivo')
+            }else {
+                console.log(`Se guardó el producto bajo el ID ${product.id}`)
+            }
+        })
+    }
+
+    getByID(id){
+        let list = this.getAll();
+        console.log(list.find(el => el.id === id))        
+    }
+
+    deleteByID(id){
+        let list2 = this.getAll();
+        let indexToSplice = list2.findIndex(el => el.id === id);
+        list2.splice(indexToSplice,1);
+        fs.writeFile(this.file, `${JSON.stringify(list2)}`, error=> {
+            if (error) {
+                console.log('Ha ocurrido un error al escribir el archivo')
+            } else {
+                console.log('Se ha guardado correctamente')
+            }
+        });
+        console.log(list2);
+    }
+
+    deleteAll(){
+        let list3 = this.getAll();
+        list3 = [];
+        fs.writeFile(this.file, `${JSON.stringify(list3)}`, error=> {
+            if (error) {
+                console.log('Ha ocurrido un error al escribir el archivo')
+            } else {
+                console.log('Se ha guardado correctamente')
+            }
+        })
+    }
+}
+
+const listado = new Contenedor('./productos.txt');
+
+app.get('/', (req, res)=>{
+    res.send('<h1 style="color:brown">Bienvenidos<h1/>')
+});
+
+
+app.get('/productos',(req,res) => {
+    res.send(`Listado de productos: ${listado.getAll()}`)
+})
+
+app.get('/productorandom', (req,res) => {
+    const randomNumber = (min, max) => {
+        return Math.floor(Math.random()*(max-min+1)+min);
+    };
+    let randomSelected = listado.getByID(1);
+    console.log(randomNumber(1,7))
+    res.send(`Se seleccionó aleatoriamente el item: ${randomSelected}`)
+})
+
+const PORT = 8080;
+
+const server = app.listen(PORT, ()=>{
+    console.log(`Server http escuchando en ${server.address().port}`)
+})
+server.on("error", error => console.log(error))
