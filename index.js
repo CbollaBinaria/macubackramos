406f2c4860f4101ee877b6bdfac18d4b9037108b
Sebastian Ramos <sebastianalejandroramos96@gmail.com>
2022-07-20 19:46:31 -0300

    Routes

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const Contenedor = require('./server');
+const listado = new Contenedor('./productos.txt');
 
 app.use(express.json());
-app.use('/api/productos', router);
-let productos =[];
+app.use('', router);
+router.use(express.urlencoded({ extended: true }));
+
+//<-------------------------------------------------------Handlebars
+
+const { engine } = require('express-handlebars')
 
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+//<--------------------------------------------------------Subida y obtención de productos
+let productos = listado.getAll();
 
 
-router.get('/', (req, res)=>{
-    res.json(productos)
+router.get('/api/productos', (req, res)=>{
+    const response =  listado.getAll()
+    res.render('products', {response})
 });
 
-router.get('/:id', (req, res)=> {
+
+
+
+router.get('/api/productos/:id', (req, res)=> {
     let id = req.params.id;
     let producto = productos.find(el => el.id == id);
     res.json(producto);
 });
 
-router.post('/', (req, res) => {
+router.post('/api/productos/', (req, res) => {
     let producto = req.body;
-    producto.id = productos.length +1;
-    productos.push(producto);
-    res.json(producto)
+    listado.saveProduct(producto)
+    res.redirect('/api/productos/')
 })
 
 
--- server.js
+++ server.js
@@ -1,72 +1,59 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-// class Contenedor {
-//     constructor(file){
-//         this.file = file
-//     }
+class Contenedor {
+    constructor(file){
+        this.file = file
+    }
 
-//     getAll(){
-//         const data = JSON.parse(fs.readFileSync(this.file, 'utf-8'))
-//         console.log(data)
-//         return data
-//     }
-//     saveProduct(product){
-//         let listado = this.getAll();
-//         product.id = listado.length+1        
-//         listado.push(product)
-//         fs.writeFile(this.file, `${JSON.stringify(listado)}`, error => {
-//             if (error){
-//                 console.log('Hubo un problema al guardar el archivo')
-//             }else {
-//                 console.log(`Se guardó el producto bajo el ID ${product.id}`)
-//             }
-//         })
-//     }
+    getAll(){
+        const data = JSON.parse(fs.readFileSync(this.file, 'utf-8'))
+        console.log(data)
+        return data
+    }
+    saveProduct(product){
+        let listado = this.getAll();
+        product.id = listado.length+1        
+        listado.push(product)
+        fs.writeFile(this.file, `${JSON.stringify(listado)}`, error => {
+            if (error){
+                console.log('Hubo un problema al guardar el archivo')
+            }else {
+                console.log(`Se guardó el producto bajo el ID ${product.id}`)
+            }
+        })
+    }
 
-//     getByID(id){
-//         let list = this.getAll();
-//         console.log(list.find(el => el.id === id))        
-//     }
+    getByID(id){
+        let list = this.getAll();
+        console.log(list.find(el => el.id === id))        
+    }
 
-//     deleteByID(id){
-//         let list2 = this.getAll();
-//         let indexToSplice = list2.findIndex(el => el.id === id);
-//         list2.splice(indexToSplice,1);
-//         fs.writeFile(this.file, `${JSON.stringify(list2)}`, error=> {
-//             if (error) {
-//                 console.log('Ha ocurrido un error al escribir el archivo')
-//             } else {
-//                 console.log('Se ha guardado correctamente')
-//             }
-//         });
-//         console.log(list2);
-//     }
+    deleteByID(id){
+        let list2 = this.getAll();
+        let indexToSplice = list2.findIndex(el => el.id === id);
+        list2.splice(indexToSplice,1);
+        fs.writeFile(this.file, `${JSON.stringify(list2)}`, error=> {
+            if (error) {
+                console.log('Ha ocurrido un error al escribir el archivo')
+            } else {
+                console.log('Se ha guardado correctamente')
+            }
+        });
+        console.log(list2);
+    }
 
-//     deleteAll(){
-//         let list3 = this.getAll();
-//         list3 = [];
-//         fs.writeFile(this.file, `${JSON.stringify(list3)}`, error=> {
-//             if (error) {
-//                 console.log('Ha ocurrido un error al escribir el archivo')
-//             } else {
-//                 console.log('Se ha guardado correctamente')
-//             }
-//         })
-//     }
-// }
+    deleteAll(){
+        let list3 = this.getAll();
+        list3 = [];
+        fs.writeFile(this.file, `${JSON.stringify(list3)}`, error=> {
+            if (error) {
+                console.log('Ha ocurrido un error al escribir el archivo')
+            } else {
+                console.log('Se ha guardado correctamente')
+            }
+        })
+    }
+}
 
+module.exports = Contenedor;
 
-
-// const zapatillas = {
-//     title: 'Zapatillas',
-//     price: 7500,
-//     url: 'zapatillas.jpg'
-//     }
-
-// const listado = new Contenedor('./productos.txt');
-
-
-// // listado.saveProduct(zapatillas);
-// // listado.getByID(5);
-// // listado.deleteByID(2);
-// //listado.deleteAll();
